c5f2ed2deceb4582b85fe00aada31c19b93f53c6
Zhao Yu <zhaolyu@gmail.com>
2016-04-26 21:22:17 -0400

    do while fix

--- MyConsoleMenu.java
+++ MyConsoleMenu.java
@@ -4,39 +4,26 @@
 public class MyConsoleMenu extends ConsoleMenu  {
     MyConsoleMenu() {
         System.out.print("Welcome to MAC Sun Financial Tools \nOur tools can help you find Bullish and Bearish engulfings. \n\n");
-        
-        System.out.println("Options:");
-        System.out.println("---------------------------------");
-        System.out.println("1) Download Csv file from Yahoo");
-        System.out.println("2) Enter File Path from computer");
-        this.option = inputInteger("Pick an option: ", "You haven't chosen any option ");
-        
-        while( this.option > 2 ) {
-	        System.out.println("\nOptions:");
-	        System.out.println("-------------------------------------");
-	        System.out.println("1) Download Csv file from Yahoo");
-	        System.out.println("2) Enter File Path from computer \n");
-	        this.option = inputInteger("Pick one of options above: ", "You haven't chosen any option");
-        }
-       
+
+        do {
+            System.out.println("\nOptions:");
+            System.out.println("-------------------------------------");
+            System.out.println("1) Download Csv file from Yahoo");
+            System.out.println("2) Enter File Path from computer \n");
+            this.option = inputInteger("Pick an option: ", "You haven't chosen any option ");
+        } while (this.option > 2)
+
         firstOption();
-        
-        System.out.println("\nOutput Options:");
-        System.out.println("------------------------------------------------------------");
-        System.out.println("1) Print table in console of the next 10 days");
-        System.out.println("2) Create Csv File of the next 10 days profit %");
-        System.out.println("3) Print table in console and Write Csv file of the 10 days profit %");
-        this.outputOption = inputInteger("Pick an output Option:", "You haven't chosen any option");
-        
-        while( this.outputOption > 3) {
+
+        do {
             System.out.println("\nOutput Options:");
             System.out.println("-------------------------------------------------------------");
             System.out.println("1) Print table in console of the next 10 days");
             System.out.println("2) Create Csv File of the next 10 days profit %");
             System.out.println("3) Print table in console and Write Csv file of the 10 days profit %");
             this.outputOption = inputInteger("Pick an output Option from above:", "You haven't chosen any option");
-        }
-        
+        } while( this.outputOption > 3)
+
         secondOption();
     }
 
